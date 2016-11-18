2185f479d2f8ae819562248ad1ed19ab11e5e0cf
Joe <jwataru@gmail.com>
2016-11-19 09:14:31 +1100

    Apply design.

--- index.android.js
+++ index.android.js
@@ -1,13 +1,6 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import TimerMixin from 'react-timer-mixin';
 import { View, Text, StyleSheet, AppRegistry, TouchableHighlight } from 'react-native';
-
 
 class SampleTimer extends Component{
 
@@ -23,67 +16,47 @@ class SampleTimer extends Component{
     this._resetTimer = this._resetTimer.bind(this);
   }
 
-  componentDidMount() {
-  }
-
   _updateTimer() {
-    this.setState({ currentTimer: this.state.currentTimer+1 });
+    this.setState({ currentTimer: this.state.currentTimer + 1 });
   }
 
   _startTimer() {
-    //alert('Timer start...');
-    //TimerMixin.setInterval( () => this._updateTimer(), 500);
-
-    // TimerMixin.setInterval( () => {
-    //   this.setState({
-    //     currentTimer: this.state.currentTimer+1
-    //   })
-    // }, 500);
-
-    //this.timer = setInterval( () => this._updateTimer(), 500);
-
     this.timer = TimerMixin.setInterval( () => this._updateTimer(), 500);
-
   }
 
-
-
   _stopTimer() {
-    //alert('Timer stop...');
-    //TimerMixin.clearInterval('mytimer');
-
     clearInterval(this.timer);
   }
 
   _resetTimer() {
-    //alert('Timer stop...');
-    //TimerMixin.clearInterval('mytimer');
-
+    this._stopTimer();
     this.setState({currentTimer: 0});
   }
 
   render() {
     return (
       <View style={styles.container}>
-       <View>
-         <Text>TIMER - {this.state.currentTimer}
-         </Text>
+        <View style={styles.displayContainer}>
+          <Text style={styles.display}> {this.state.currentTimer}
+          </Text>
         </View>
-        <TouchableHighlight onPress={this._startTimer}>
-          <Text>
-            Start
-          </Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this._stopTimer}>
-          <Text>
-            Stop
-          </Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this._resetTimer}>
-          <Text>
-            Reset
-          </Text>
-        </TouchableHighlight>
+        <View style={styles.buttonContainer}>
+          <TouchableHighlight style={styles.button} onPress={this._startTimer}>
+            <Text style={styles.text}>
+              Start
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.button} onPress={this._stopTimer}>
+            <Text style={styles.text}>
+              Stop
+            </Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={styles.button} onPress={this._resetTimer}>
+            <Text style={styles.text}>
+              Reset
+            </Text>
+          </TouchableHighlight>
+        </View>
       </View>
     );
   }
@@ -96,6 +69,32 @@ var styles = StyleSheet.create({
     flex: 1,
     marginTop:60,
   },
+  displayContainer: {
+    flex: 2,
+    justifyContent: 'center',
+  },
+  display: {
+    fontSize: 80,
+    textAlign: 'center',
+  },
+  buttonContainer: {
+    flex: 2,
+    flexDirection: 'row',
+  },
+  button: {
+    flex: 1,
+    backgroundColor: 'steelblue',
+    borderColor: 'steelblue',
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 5,
+    height: 30,
+  },
+  text: {
+    color: '#ffffff',
+    textAlign: 'center',
+    marginTop: 3,
+  }
 });
 
 AppRegistry.registerComponent('SampleTimer', () => SampleTimer);
